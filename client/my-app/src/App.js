3d4fbde48429c4676e1c38ce8b7f4b2ff24589cb
shreayasinghkunwar <shreaya@fusemachines.com>
2022-11-05 19:47:33 +0545

    Navbar component

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-     <h1>hiii</h1>
+      <Navbar></Navbar>
+      <Home></Home>
     </div>
   );
 }
